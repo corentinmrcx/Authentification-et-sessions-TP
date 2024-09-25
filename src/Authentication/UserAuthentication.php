db52a546d394ffe96fa882268f213043d9ec0b89
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-09-25 11:07:36 +0200

    Modification de la classe pour intégrer les sessions

--- src/Authentication/UserAuthentication.php
+++ src/Authentication/UserAuthentication.php
@@ -7,11 +7,16 @@ namespace Authentication;
 use Authentication\Exception\AuthenticationException;
 use Entity\Exception\EntityNotFoundException;
 use Entity\User;
+use Service\Exception\SessionException;
+use Service\Session;
 
 class UserAuthentication
 {
     private const LOGIN_INPUT_NAME = 'login';
     private const PASSWORD_INPUT_NAME = 'password';
+    private const SESSION_KEY = '__UserAuthentication__';
+    private const SESSION_USER_KEY = 'user';
+    private ?User $user = null;
 
     public function loginForm(string $action, string $submitText = 'OK'): string
     {
@@ -27,6 +32,9 @@ class UserAuthentication
 HTML;
     }
 
+    /**
+     * @throws AuthenticationException
+     */
     public function getUserFromAuth(): User
     {
         $login = $_POST[self::LOGIN_INPUT_NAME];
@@ -34,10 +42,31 @@ HTML;
 
         try {
             $user = User::findByCredentials($login, $password);
+
+            $this -> setUser($user);
         } catch (EntityNotFoundException) {
             throw new AuthenticationException("Aucun utilisateur n'est trouvé !");
         }
 
         return $user;
+    }
+
+    /**
+     * @throws SessionException
+     */
+    public function setUser(User $user): void{
+        $this->user = $user;
+        Session::start();
+        $_SESSION[self::SESSION_KEY][self::SESSION_USER_KEY] = $user;
+    }
+
+    public function isUserConnected(): bool{
+        Session::start();
+        $res = false;
+        $session = $_SESSION[self::SESSION_KEY][self::SESSION_USER_KEY];
+        if (isset($session) && $session instanceof User){
+            $res = true;
+        }
+        return $res;
     }
 }
