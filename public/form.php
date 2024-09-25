675fc8937931cb212c783c150a0c752cda6a8ad8
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-09-25 12:02:40 +0200

    Modification du form.php pour ajouter le formulaire de déconnexion

--- public/form.php
+++ public/form.php
@@ -8,6 +8,9 @@ use Html\AppWebPage;
 // Création de l'authentification
 $authentication = new UserAuthentication();
 
+// Si on demande une déconnexion : des que le bouton deconnexion est cliquer
+$authentication->logoutIfRequested();
+
 $p = new AppWebPage('Authentification');
 
 // Production du formulaire de connexion
@@ -18,6 +21,11 @@ $p->appendCSS(<<<CSS
 CSS
 );
 $form = $authentication->loginForm('auth.php');
+if ($authentication->isUserConnected()){
+    // Le action doit etre en corrélation avec le fichier dans lequel on est.
+    $form = $authentication->logoutForm('form.php', "Deconnexion");
+}
+
 $p->appendContent(<<<HTML
     {$form}
     <p>Pour faire un test : essai/toto
